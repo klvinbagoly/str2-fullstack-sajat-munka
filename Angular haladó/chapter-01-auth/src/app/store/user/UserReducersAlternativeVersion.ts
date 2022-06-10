74f0c9cdba4b9cf9051d1c7f84962da9cfc38595
klvinbagoly <kbalazsk1@gmail.com>
2022-06-10 09:34:51 +0200

    alternative version of reducer

---
+++ Angular haladó/chapter-01-auth/src/app/store/user/UserReducersAlternativeVersion.ts
@@ -0,0 +1,36 @@
+import { loadItems, errorItem } from './UserActions';
+import { User } from "src/app/model/user";
+import { createReducer, on } from '@ngrx/store';
+import { tap } from 'rxjs';
+
+
+export interface State { items: User[], error: string }
+
+
+export const initialState: State = { items: [], error: '' }
+
+
+// ActionReducer: Az eseményekből eltárolja az adatokat a Store-ban.
+export const UserReducer = createReducer(
+  initialState,
+  // Eseményfigyelő függvény: eltárolja az adatokat a Store-ban.
+  on(loadItems, (state, action) => {
+    console.log(state)
+    return {
+      ...state,
+      items: action.items
+    }
+  }),
+  on(errorItem, (state, action) => ({
+    ...state,
+    error: action.message
+  }))
+)
+
+// A selectorok lekérdezik az adatokat a Store-ból.
+
+export const selectError = (states: { [key: string]: State }) => states['users'].error
+export const selectItems = (states: { [key: string]: State }) => {
+  console.log(states)
+  return states['users'].items
+}
